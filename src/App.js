e6e2f2beb667d75c49d3272cbadd4c059cca6550
Artem Shkuratov <shuratov2001@mail.ru>
2021-03-19 03:38:26 +0800

    create start structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Определение вида треугольниика по заданным сторонам</h1>
+       <hr/>
       </header>
+      <div className="App-contener">
+        <div className="App-contener__item">
+          <div className="item">
+            <label>Длинна стороны 1</label>
+            <input type="text"></input>
+          </div>
+          <div className="item">
+            <label>Длинна стороны 2</label>
+            <input type="text"></input>
+          </div>
+          <div className="item">
+            <label>Длинна стороны 3</label>
+            <input type="text"></input>
+          </div>
+          <hr/>
+          <div className="item">
+            <button>Вычислить</button>
+          </div>
+          <hr/>
+        </div>
+        <div className="App-contener_results">
+          <div className="item">
+            <input type="text"></input>
+          </div>
+          <hr/>
+          <div className="item">
+            <button>Reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
