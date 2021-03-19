47f930ddef53facbed870fc3dcd4d00e64d7afc5
Artem Shkuratov <shuratov2001@mail.ru>
2021-03-19 19:36:39 +0800

    added isosceles function to triangle

--- src/tests/triangleFunctionTests.js
+++ src/tests/triangleFunctionTests.js
@@ -25,16 +25,27 @@ const versatile = (a, b, c) =>{
     }
     return result
 }
+const isosceles = (a, b, c) =>{
+    let result = ""
+    if (a == b || b == c || a == c){
+        result = true
+    }else{
+        result = false
+    }
+    return result
+}
 export const typeOfTriangle = (a, b, c) =>{
     let res = { 
         equilateral: "",
         noTriangle: "",
-        versatile: ""
+        versatile: "",
+        isosceles: ""
     }
     let triangleType = ''
     res.equilateral = equilateral(a, b, c)
     res.noTriangle = noTriangle(a, b, c)
     res.versatile = versatile(a, b, c)
+    res.isosceles = isosceles(a, b, c)
     
     if (res.equilateral){
         triangleType = "Равносторонний"
@@ -42,6 +53,8 @@ export const typeOfTriangle = (a, b, c) =>{
         triangleType = "Фигура не является треугольником"
     }else if (res.versatile){
         triangleType = "Разносторонний"
+    }else if (res.isosceles){
+        triangleType = "Равнобедренный"
     }
     
     return triangleType
